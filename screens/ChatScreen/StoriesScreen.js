2cad622e8dfc23558bd732d1e5ae749b2c68aefb
Ashish <ashishmp0471@gmail.com>
2020-05-09 22:33:49 +0530

    Updated StoriesScreen.js

--- screens/ChatScreen/StoriesScreen.js
+++ screens/ChatScreen/StoriesScreen.js
@@ -1,11 +1,98 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,ScrollView,ImageBackground } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+const images = [
+    {   
+        name:'Ashish',
+        uri:["https://images.unsplash.com/photo-1556740749-887f6717d7e4",
+             "https://images.unsplash.com/photo-1556740749-887f6717d7e4"]
+    },
+    {
+        name:'Aaron',
+        uri:["https://images.unsplash.com/photo-1556740749-887f6717d7e4",
+            "https://images.unsplash.com/photo-1556740749-887f6717d7e4"]
+    },
+    {
+        name:'Ronaldo',
+        uri:["https://images.unsplash.com/photo-1556740749-887f6717d7e4",
+            "https://images.unsplash.com/photo-1556740749-887f6717d7e4"]
+    },
+    {
+        name:'Messi',
+        uri:["https://images.unsplash.com/photo-1556740749-887f6717d7e4",
+            "https://images.unsplash.com/photo-1556740749-887f6717d7e4"]
+    },
+  ];
 
 const StoriesScreen = props => {
     return (
+        <ScrollView showsVerticalScrollIndicator={false}>
         <View style = {styles.coontainer} >
-            <Text>Stories Screen</Text>
+            <View style = {styles.addStory}>
+                <TouchableOpacity>
+                    <View style = {styles.addStoryBox}>
+                        <Ionicons name = "md-add" size = {34} color = 'grey'/>
+                    </View>
+                </TouchableOpacity>
+                <View style = {{marginLeft:15}}>
+                    <Text style = {{fontSize:16,fontWeight:'bold',marginBottom:10}}>My Story</Text>
+                    <Text>Add to my Story</Text>
+                </View>
+            </View>
+            <View style = {{marginTop:30}}>
+                <Text style = {{fontSize:14,fontWeight:'700',color:'grey'}}>
+                    RECENT STORIES
+                </Text>
+                <View style = {{marginTop:10}}>
+                {images.map((image, imageIndex) => {
+            return ( 
+              <View style = {{marginVertical:10}}
+                key={imageIndex}
+              > 
+                
+                <View style = {{flexDirection:'row'}}>
+                <ImageBackground source={{ uri: image.uri[0] }} style={styles.card}>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.infoText}>
+                      {image.name}
+                    </Text>
+                  </View>
+                </ImageBackground>
+                </View>
+              </View>
+            );
+          })}  
+                </View>
+            </View>
+            <View style = {{marginTop:30}}>
+                <Text style = {{fontSize:14,fontWeight:'700',color:'grey'}}>
+                    VIEWED STORIES
+                </Text>
+                <View style = {{marginTop:10}}>
+                {images.map((image, imageIndex) => {
+            return (
+                //Container here 
+              <View style = {{marginVertical:10}}
+                key={imageIndex}
+              > 
+                <View style = {{flexDirection:'row'}}>
+                <ImageBackground source={{ uri: image.uri[0] }} style={styles.card}>
+                  <View style={styles.textContainer}>
+                    <Text style={styles.infoText}>
+                      {image.name}
+                    </Text>
+                  </View>
+                </ImageBackground>
+                </View>
+              </View>
+            );
+          })}  
+                </View>
+            </View>
+        
         </View>
+        </ScrollView>
     );
 }
 
@@ -17,16 +104,52 @@ StoriesScreen.navigationOptions = navData => {
         },
         headerTitle: 'Stories',
         headerTitleAlign: 'center',
-        headerTintColor: 'white'
+        headerTintColor: 'white',
     }
 }
 
 const styles = StyleSheet.create({
     coontainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+        flexDirection:'column',
+        alignItems: 'flex-start',
+        paddingLeft:30,
+        paddingTop:30,
+    },
+    addStory:{
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center'
+
+    },
+    addStoryBox:{
+        width:80,
+        height:110,
+        borderWidth:0.8,
+        borderRadius:5,
+        justifyContent:'center',
+        alignItems:'center',
+        borderColor:'grey'
+    },
+    card: {
+        flex: 1,
+        marginVertical: 4,
+        borderRadius: 5,
+        overflow: "hidden",
+        alignItems: "center",
+        justifyContent: "center"
+      },
+      textContainer: {
+        backgroundColor: "rgba(0,0,0, 0.7)",
+        paddingHorizontal: 24,
+        paddingVertical: 8,
+        borderRadius: 5
+      },
+      infoText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold"
+      },
 })
 
 export default StoriesScreen;
