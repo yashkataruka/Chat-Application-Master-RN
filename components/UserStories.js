fd23430f98c7d7b0de4446b881717a93069f390e
AnshikaAgrawal <anshikaagrawal1089@gmail.com>
2020-05-09 19:15:44 +0530

    UserStories.js completed for images. view videos left

--- components/UserStories.js
+++ components/UserStories.js
@@ -5,51 +5,34 @@ import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 
 class UserStories extends React.Component {
   renderStories = () => {
-    let stories = []
-    if (this.props.flag) {
-      for (let i = 0; i < 5; i++) {
-        stories.push(
-          <Image
-            source={{ uri: 'https://scontent.fdel21-1.fna.fbcdn.net/v/t31.0-0/p640x640/27500624_1631883743526849_5371185179369120659_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=HxC82h-6TjgAX9PHczR&_nc_ht=scontent.fdel21-1.fna&_nc_tp=6&oh=433e35c82605599e6def6a9d1ce587d5&oe=5ED304FC' }}
-            style={styles.story}
-          />
-        )
-      }
-    } else {
-      stories.push(
+    return this.props.stories.map(img => {
+      return (
         <Image
-          source={{ uri: 'https://scontent.fdel21-1.fna.fbcdn.net/v/t31.0-0/p640x640/27500624_1631883743526849_5371185179369120659_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=HxC82h-6TjgAX9PHczR&_nc_ht=scontent.fdel21-1.fna&_nc_tp=6&oh=433e35c82605599e6def6a9d1ce587d5&oe=5ED304FC' }}
+          source={{ uri: img }}
           style={styles.story}
         />
       )
-    }
-    return stories;
+    })
   }
   render() {
     return (
       <View style={styles.mainView}>
         <View style={styles.upper}>
           <Image
-            source={{ uri: 'https://scontent.fdel21-1.fna.fbcdn.net/v/t31.0-0/p640x640/27500624_1631883743526849_5371185179369120659_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=HxC82h-6TjgAX9PHczR&_nc_ht=scontent.fdel21-1.fna&_nc_tp=6&oh=433e35c82605599e6def6a9d1ce587d5&oe=5ED304FC' }}
+            source={{ uri: this.props.image }}
             style={styles.image}
           />
-          <Text style={styles.username}>Yash Kataruka</Text>
+          <Text style={styles.username}>{this.props.name}</Text>
           <Text style={styles.rightInfo}>date and time</Text>
         </View>
         <ScrollView
-          style={styles.lower} horizontal={true}
+          style={styles.lower}
+          horizontal={true}
           showsHorizontalScrollIndicator={false}
-        // ref={ref => { this.scrollView = ref }}
-        // onContentSizeChange={() => this.scrollView.scrollToEnd({ animated: true })}
         >
-          <Image
-            source={{ uri: 'https://scontent.fdel21-1.fna.fbcdn.net/v/t31.0-0/p640x640/27500624_1631883743526849_5371185179369120659_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=HxC82h-6TjgAX9PHczR&_nc_ht=scontent.fdel21-1.fna&_nc_tp=6&oh=433e35c82605599e6def6a9d1ce587d5&oe=5ED304FC' }}
-            style={styles.story}
-          />
+          {this.renderStories()}
 
-          />
         </ScrollView>
-
       </View>
     )
   }
@@ -57,7 +40,7 @@ class UserStories extends React.Component {
 
 const styles = StyleSheet.create({
   mainView: {
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   upper: {
     flexDirection: 'row',
@@ -74,7 +57,7 @@ const styles = StyleSheet.create({
   },
   username: {
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: "400",
     alignSelf: 'center',
     color: '#525151',
   },
--- screens/ChatScreen/StoriesScreen.js
+++ screens/ChatScreen/StoriesScreen.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import UserStories from "../../components/UserStories"
+import UserStories from "../../components/UserStories";
+
+//random Samples
+const img1 = "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+const img2 = "https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=282&q=80"
+const img3 = "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
 
 const StoriesScreen = props => {
-    let img1 = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.adobe.com%2Fcontent%2Fdam%2Fcc%2Fus%2Fen%2Fcreativecloud%2Fphotography%2Fdiscover%2Fportrait-photography%2FCODERED_B1_portrait_photography-P4a_438x447.jpg.img.jpg&imgrefurl=https%3A%2F%2Fwww.adobe.com%2Fcreativecloud%2Fphotography%2Fdiscover%2Fportrait-photography.html&tbnid=rDVnn182Xs7LUM&vet=12ahUKEwj18urO56bpAhUzWHwKHZY1BfUQMygIegUIARCrAg..i&docid=yJ4FxNwCujeqbM&w=438&h=447&q=portrait&ved=2ahUKEwj18urO56bpAhUzWHwKHZY1BfUQMygIegUIARCrAg"
-    let img2 = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmymodernmet.com%2Fwp%2Fwp-content%2Fuploads%2F2019%2F07%2Fai-portraits-celebrities-keanu-reeves.jpg&imgrefurl=https%3A%2F%2Fmymodernmet.com%2Fai-portraits%2F&tbnid=FBOTdnOrzo47QM&vet=12ahUKEwj18urO56bpAhUzWHwKHZY1BfUQMygLegUIARCxAg..i&docid=BTF9HuCCGVyA8M&w=750&h=375&q=portrait&ved=2ahUKEwj18urO56bpAhUzWHwKHZY1BfUQMygLegUIARCxAg"
-    let img3 = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1506794778202-cad84cf45f1d%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjEyMDd9%26w%3D1000%26q%3D80&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fportrait&tbnid=g3rPcS0txcWl4M&vet=12ahUKEwj18urO56bpAhUzWHwKHZY1BfUQMygPegUIARC5Ag..i&docid=INW7sO3zOe1OeM&w=1000&h=1500&q=portrait&ved=2ahUKEwj18urO56bpAhUzWHwKHZY1BfUQMygPegUIARC5Ag"
     return (
         <View style={styles.coontainer} >
-            <UserStories flag="true" image={img1} />
-            <UserStories image={img2} />
-            <UserStories image={img3} />
+            <UserStories flag="true" image={img1} name="Tara Hussain" stories={[img1, img2, img3, img2, img3, img1]} />
+            <UserStories image={img2} name="Himani Malkeshakr" stories={[img1, img2, img3, img2]} />
+            <UserStories image={img3} name="Joy S.Rohank" stories={[img1, img2]} />
         </View>
     );
 }
